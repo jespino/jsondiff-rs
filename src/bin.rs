54eab2588769d598d24eb34bc2e7ac7eb702103f
Jesús Espino <jespinog@gmail.com>
2016-07-23 23:29:50 +0200

    Multithreaded file loading

--- src/bin.rs
+++ src/bin.rs
@@ -9,15 +9,18 @@ extern crate jsondiff;
 extern crate log;
 extern crate loggerv;
 
+use std::thread;
 use clap::{Arg, App};
 use serde_json::ser::to_string_pretty;
 use serde_json::de::from_reader;
 use std::io::prelude::*;
 use std::fs::File;
 use jsondiff::{diff, similarity};
+use std::sync::Arc;
+
 
 fn main() {
-    let matches = App::new("JsonDiff")
+    let matches = Arc::new(App::new("JsonDiff")
                           .version("1.0")
                           .author("Jesús Espino <jespinog@gmail.com>")
                           .about("Generate the difference between 2 json")
@@ -43,23 +46,34 @@ fn main() {
                                .value_name("FILE")
                                .help("Sets the output file to use")
                                .takes_value(true))
-                          .get_matches();
+                          .get_matches());
 
 	loggerv::init_with_verbosity(matches.occurrences_of("verbosity")).unwrap();
 
-	let input1 = matches.value_of("INPUT1").unwrap();
-    info!("loading file {}", input1);
-    let data1 = match input1 {
-        "-" => from_reader(std::io::stdin()).unwrap(),
-        value => from_reader(File::open(value).unwrap()).unwrap()
-    };
+    let input1 = Arc::new(matches.value_of("INPUT1").unwrap().to_owned());
+    let input2 = Arc::new(matches.value_of("INPUT2").unwrap().to_owned());
 
-	let input2 = matches.value_of("INPUT2").unwrap();
-    info!("loading file {}", input2);
-    let data2 = match input2 {
-        "-" => from_reader(std::io::stdin()).unwrap(),
-        value => from_reader(File::open(value).unwrap()).unwrap()
-    };
+    info!("loading file {}", *input1);
+    let load1_thread = thread::spawn(move || {
+        if *input1 == "-".to_string() {
+            from_reader(std::io::stdin()).unwrap()
+        } else {
+            from_reader(File::open(&*input1).unwrap()).unwrap()
+        }
+    });
+
+    info!("loading file {}", *input2);
+    let load2_thread = thread::spawn(move || {
+        if *input2 == "-".to_string() {
+            from_reader(std::io::stdin()).unwrap()
+        } else {
+            from_reader(File::open(&*input2).unwrap()).unwrap()
+        }
+    });
+
+    let data1 = load1_thread.join().unwrap();
+    let data2 = load2_thread.join().unwrap();
+
 
     info!("processing");
     if matches.is_present("similarity") {
